04de99ae9651c10176f5b5d1750c12fc9b396070
edel08 <edel.william@outlook.com>
2022-09-30 15:23:36 +0700

    1. Repository for Database Training 2 (30/9/2022 3:21 PM)

---
+++ VersionSQL/DESKTOP-GL0DDFA/SQLEXPRESS/Training2/dbo/Table/Total_Purchase.sql
@@ -0,0 +1,11 @@
+﻿/****** Object:  Table [dbo].[Total_Purchase]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[Total_Purchase](
+	[Year] [float] NULL,
+	[Quarter] [float] NULL,
+	[Vendor_Name] [nvarchar](255) NULL,
+	[Total_Purchase_Store] [float] NULL,
+	[Filename] [nvarchar](255) NULL
+) ON [PRIMARY]
